e98672d22b2a2777f2385809476667fd6f5131f7
Chris Smeal <chris.smeal@synapsewireless.com>
2019-07-24 23:00:36 -0500

    Basic init.  Searches one day.

---
+++ thing.py
@@ -0,0 +1,31 @@
+import requests
+
+FORMAT_TYPES = ['pauper', 'modern', 'legacy', 'vintage', 'standard']
+LEAGUE_TYPES = ['mcq','mocs', 'league', 'challenge']
+STARTING_URL = 'https://magic.wizards.com/en/articles/archive/mtgo-standings/'
+
+
+def get_all(start_year, start_month, start_day, end_year, end_month, end_day):
+    pass
+
+
+def get_all_for_date(year, month, day):
+    result = []
+    year = year if len(str(year)) == 4 else '20' + str(year)
+    month = month if len(str(month)) == 2 else '0' + str(month)
+    day = day if len(str(day)) == 2 else '0' + str(day)
+    date = f'{str(year)}-{str(month)}-{str(day)}'
+
+    for f in FORMAT_TYPES:
+        for t in LEAGUE_TYPES:
+          url = f'{STARTING_URL}{f}-{t}-{date}'
+          r = requests.get(url)
+          if r.status_code == 200 and len(r.text) > 90000:
+            result.append({
+              'date': date,
+              'url': url, 
+              'format_type': f
+            })
+          
+
+    return result
