992f81f40fe0197baf381faa4a32372e1e94bee0
Chris Smeal <chris.smeal@synapsewireless.com>
2019-07-30 20:52:57 -0500

    Add looping.

--- thing.py
+++ thing.py
@@ -1,12 +1,16 @@
 import requests
 
 FORMAT_TYPES = ['pauper', 'modern', 'legacy', 'vintage', 'standard']
-LEAGUE_TYPES = ['mcq','mocs', 'league', 'challenge']
+LEAGUE_TYPES = ['mcq', 'mocs', 'league', 'challenge']
 STARTING_URL = 'https://magic.wizards.com/en/articles/archive/mtgo-standings/'
 
 
 def get_all(start_year, start_month, start_day, end_year, end_month, end_day):
-    pass
+    res = []
+    for year in range(start_year, end_year):
+        for month in range(start_month, end_month):
+            for day in range(start_day, end_day):
+                res.append(year, month, day)
 
 
 def get_all_for_date(year, month, day):
@@ -18,14 +22,13 @@ def get_all_for_date(year, month, day):
 
     for f in FORMAT_TYPES:
         for t in LEAGUE_TYPES:
-          url = f'{STARTING_URL}{f}-{t}-{date}'
-          r = requests.get(url)
-          if r.status_code == 200 and len(r.text) > 90000:
-            result.append({
-              'date': date,
-              'url': url, 
-              'format_type': f
-            })
-          
+            url = f'{STARTING_URL}{f}-{t}-{date}'
+            r = requests.get(url)
+            if r.status_code == 200 and len(r.text) > 90000:
+                result.append({
+                    'date': date,
+                    'url': url,
+                    'format_type': f
+                })
 
     return result
